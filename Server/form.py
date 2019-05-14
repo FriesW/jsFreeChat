6670822c044535545968fe1b4453f141c4d7e56c
FriesW <william.james.fries@gmail.com>
2019-05-13 23:24:05 -0500

    Form resubmission workaround

--- Server/form.py
+++ Server/form.py
@@ -3,10 +3,14 @@ from sanic import response
 
 app = Sanic('Main')
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 async def handler(request):
+    return await response.file('submit.html')
+
+@app.route('/', methods=['POST'])
+async def p_handler(request):
     if 'msg' in request.form:
         print(request.form.get('msg'))
-    return await response.file('submit.html')
+    return response.redirect('/')
 
 app.run('0.0.0.0',8000)
